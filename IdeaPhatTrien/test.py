3335a33e8888aa35da7fc691bb1b41bed6a7eaf8
Phu Dinh <you@>
2021-05-02 01:29:17 +0700

    got idea about gpu and cthon

--- IdeaPhatTrien/test.py
+++ IdeaPhatTrien/test.py
@@ -1,13 +1,60 @@
-import cv2
+import numpy as np
+from timeit import default_timer as timer
+from numba import vectorize
 
-img = cv2.imread("image.png", cv2.IMREAD_GRAYSCALE)
-src = cv2.cuda_GpuMat()
-src.upload(img)
+# This should be a substantially high value. On my test machine, this took
+# 33 seconds to run via the CPU and just over 3 seconds on the GPU.
+NUM_ELEMENTS = 100000000
 
-clahe = cv2.cuda.createCLAHE(clipLimit=5.0, tileGridSize=(8, 8))
-dst = clahe.apply(src, cv2.cuda_Stream.Null())
 
-result = dst.download()
+# This is the CPU version.
+def vector_add_cpu(a, b):
+    c = np.zeros(NUM_ELEMENTS, dtype=np.float32)
+    for i in range(NUM_ELEMENTS):
+        c[i] = a[i] + b[i]
+    return c
 
-cv2.imshow("result", result)
-cv2.waitKey(0)
+
+# This is the GPU version. Note the @vectorize decorator. This tells
+# numba to turn this into a GPU vectorized function.
+@vectorize(["float32(float32, float32)"], target='cuda')
+def vector_add_gpu(a, b):
+    return a + b
+
+
+def main():
+    a_source = np.ones(NUM_ELEMENTS, dtype=np.float32)
+    b_source = np.ones(NUM_ELEMENTS, dtype=np.float32)
+
+    # Time the CPU function
+    start = timer()
+    vector_add_cpu(a_source, b_source)
+    vector_add_cpu_time = timer() - start
+
+    # Time the GPU function
+    start = timer()
+    vector_add_gpu(a_source, b_source)
+    vector_add_gpu_time = timer() - start
+
+    # Report times
+    print("CPU function took %f seconds." % vector_add_cpu_time)
+    print("GPU function took %f seconds." % vector_add_gpu_time)
+
+    return 0
+
+
+if __name__ == "__main__":
+    print("hh")
+    main()
+
+cdef f(double x):
+    return 2*x*x + 3*x + 1
+def trapez(double a, double b, int n):
+    cdef double h = (b-a)/n
+    cdef double sum = 0, x = a
+    cdef int i
+    for i in range(n-1):
+    x += h
+        sum += f(x)
+        sum += 0.5*(f(a) + f(b))
+    return sum*h
